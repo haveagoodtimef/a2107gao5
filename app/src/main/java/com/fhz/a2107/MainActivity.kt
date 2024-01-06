cfbeb1535d8e103642817f5474bf35acb8f8118a
feng <78236491@qq.com>
2024-01-06 10:49:28 +0800

    创建空项目

---
+++ app/src/main/java/com/fhz/a2107/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fhz.a2107
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
